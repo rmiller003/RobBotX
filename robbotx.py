9b2eb608399a525cdc901dc20da2b398df0e0d12
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-04-25 22:06:39 -0400

    Stage 1

---
+++ robbotx.py
@@ -0,0 +1,25 @@
+# This is a simple chatbot app programmed in Python 3.9 using tkinter for a GUI interface
+# Written by Robert Miller
+
+from tkinter import *
+root=Tk()
+def send():
+    send=e.get()
+    txt.insert(END,"\n"+send)
+    if(e.get()=="Hello"):
+        txt.insert(END,"\n"+"RobBot => Hi")
+    elif(e.get()=="hi"):
+        txt.insert(END,"\n"+"RobBot => Hello")
+    elif(e.get()=="how are you"):
+        txt.insert(END,"\n"+"RobBot => I'm fine and you?")
+    if(e.get()=="fine"):
+        txt.insert(END,"\n"+"RobBot => Nice to hear")
+    e.delete(0,END)
+txt=Text(root)
+txt.grid(row=0,column=0,columnspan=2)
+e=Entry(root,width=100)
+send=Button(root,text="Send",command=send).grid(row=1,column=1)
+e.grid(row=1,column=0)
+root.title("RobBotX")
+root.mainloop()
+
