02b0d0e9772da0af487fd67814cbc787f589d325
rmiller003 <54287828+rmiller003@users.noreply.github.com>
2021-04-26 17:29:23 -0400

    Stage 3

--- robbotx.py
+++ robbotx.py
@@ -2,20 +2,38 @@
 # Written by Robert Miller
 
 from tkinter import *
+import random, datetime, calendar
+
 root=Tk()
 def send():
     send=e.get()
     txt.insert(END,"\n"+send)
     if(e.get()=="Hello"):
         txt.insert(END,"\n"+"RobBot => Hi")
-    elif(e.get()=="hi"):
+    elif(e.get()=="Hi"):
         txt.insert(END,"\n"+"RobBot => Hello")
     elif(e.get()=="how are you"):
         txt.insert(END,"\n"+"RobBot => I'm fine and you?")
     elif(e.get()=="fine"):
         txt.insert(END,"\n"+"RobBot => Nice to hear")
+    elif(e.get()=="what's your name"):
+        txt.insert(END,"\n"+"RobBot => I'm the RobBot, it's nice to meet you")
+    elif(e.get()=="I have a question"):
+        txt.insert(END,"\n"+"Ok, how can I help?")
+    elif(e.get()=="where do you live"):
+        txt.insert(END,"\n"+"9 Dunnville Rd, Robert's computer Lab, Keswick")
+    elif(e.get()=="are u a robot"):
+        txt.insert(END,"\n"+"I am an Artificial Intelligence designed to answer your questions ")
+    elif(e.get()=="what time is it"):
+        txt.insert(END,"\n"+"Please use the following link for local Eastern Standard Time > https://www.timeanddate.com/worldclock/canada/toronto")
+
+
+
+
     else:
         txt.insert(END,"\n"+"RobBot => Sorry I didn't understand that!?!?")
+
+
     e.delete(0,END)
 txt=Text(root)
 txt.grid(row=0,column=0,columnspan=2)
